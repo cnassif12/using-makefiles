9ed25bf4d8da49c7b4efb1dbc15246d35df8dc87
cnassif12 <cnassif@my.epcc.edu>
2025-11-24 21:30:42 +0000

    Completed float arithmetic library + Makefile

--- arith_utils.c
+++ arith_utils.c
@@ -3,18 +3,24 @@
 // TODO: implement each function
 
 float add(float a, float b) {
+    return a+b;
     // TODO
 }
 
 float subtract(float a, float b) {
+    return a-b;
     // TODO
 }
 
 float multiply(float a, float b) {
+    return a*b;
     // TODO
 }
 
 float divide(float a, float b) {
     // TODO
-    // if b == 0 return 0.0f
+    if (b ==0.0f){
+     return 0.0f;
+    }
+    return a/b;
 }
